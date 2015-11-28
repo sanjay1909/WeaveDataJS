e4989d68a28f47e67534ae95c471bc1da7321fd2
Sanjay krishna anbalagan <sanjay1909@gmail.com>
2015-11-28 16:50:08 -0500

    Update 2015-11-28T21:50:07.499Z

--- weavedata.js
+++ weavedata.js
@@ -5606,7 +5606,7 @@ var colorRampPresets = `<colorRampCollection>
 
     function handleDataTaskComplete() {
         // begin sorting unique strings previously listed
-        this._asyncSort.beginSort(this._uniqueStrings, weavedata.AsyncSort.compareCaseInsensitive);
+        this._asyncSort.beginSort(this._uniqueStrings, weavecore.AsyncSort.compareCaseInsensitive);
     }
 
     function handleSortComplete() {
